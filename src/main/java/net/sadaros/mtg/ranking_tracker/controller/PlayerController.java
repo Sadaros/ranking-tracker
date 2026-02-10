3877348156711583daf727682a85299e4e855480
chris <sadaros95@gmail.com>
2026-02-10 01:26:21 +0100

    Added controller for /api/players

---
+++ src/main/java/net/sadaros/mtg/ranking_tracker/controller/PlayerController.java
@@ -0,0 +1,30 @@
+package net.sadaros.mtg.ranking_tracker.controller;
+
+import net.sadaros.mtg.ranking_tracker.dto.CreatePlayerRequest;
+import net.sadaros.mtg.ranking_tracker.model.Player;
+import net.sadaros.mtg.ranking_tracker.service.PlayerService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/players")
+public class PlayerController {
+
+    private final PlayerService playerService;
+
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @GetMapping
+    public List<Player> getAllPlayers() {
+        return playerService.getAllPlayers();
+    }
+
+    @PostMapping
+    public Player createPlayer(@RequestBody CreatePlayerRequest request) {
+        return playerService.createPlayer(request.getName());
+    }
+}
---
+++ src/main/java/net/sadaros/mtg/ranking_tracker/dto/CreatePlayerRequest.java
@@ -0,0 +1,8 @@
+package net.sadaros.mtg.ranking_tracker.dto;
+
+public class CreatePlayerRequest {
+    private String name;
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
